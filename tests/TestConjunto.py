a51a435ae05ef5d8294fe9fd158411af02fbdfbb
Leonardo Araujo <74875111@continental.edu.pe>
2024-10-15 16:31:18 -0500

    Caso 1 :Calcular promedio con conjunto vacio (Rojo)

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -1,8 +1,9 @@
 import unittest
 from unittest import TestCase
-
-from src.logic import Conjunto
-
-
-class TestConjunto(unittest, TestCase):
-    pass
+from src.logic.Conjunto import Conjunto
+class TestConjunto(TestCase):
+    def test_conjunto_vacio_retornaNone(self):
+        conjunto = Conjunto([])
+        self.assertIsNone(conjunto.promedio())
+if __name__ == '__main__':
+    unittest.main()
